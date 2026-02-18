38de88cc7e8e5824a8451cbebfaba3d1ab72e2d7
Paul Nwaoduah <nwaoduahpaul@gmail.com>
2026-02-18 13:26:14 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let num1 = [50, 10, 60, 12, 20, 70];
+let scores = [80, 90, 98, 87, 100, 78, 95, 83];
+let averageS = 0;
 
-let num2 = 0;
-
-/*for(let i = 0; i < num1.length; i++){
-  num2 += num1[i];
-}*/
-
-num1.forEach(function(value){
-  num2 += value;
+scores.forEach(function(value){
+  averageS += value / scores.length;
 })
+  
+console.log(averageS.toFixed(2));
 
-console.log(num2);
+
+function averageScore(score){
+  let total = 0;
+
+  score.forEach(function(values){
+    total+=values
+  })
+  let average = total/score.length;
+  return average;
+}
+
+let student1 = [80, 40, 100, 98, 82];
+
+
+let result1 = averageScore(student1);
+console.log(`Your average score is ${result1}`);
+
+let student2 = [85, 45, 98, 83, 79];
+let result2 = averageScore(student2);
+console.log(`Your average score is ${result2}`);
